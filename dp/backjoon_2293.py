f5a591f2b22bbf8c0d6389f997e10b6a20062743
boring-km <kms0644804@naver.com>
2020-12-10 06:47:49 +0900

    [edit] 8장 다이나믹 프로그래밍
    백준 문제풀이 추가

--- dp/backjoon_2293.py
+++ dp/backjoon_2293.py
@@ -11,5 +11,14 @@ for i in range(1, n+1):
     for j in range(1, k+1):
         if j >= data[i]:
             cur[j] += cur[j-data[i]]
+    print(cur)
 print(cur[k])
 
+'''
+1 2 3 4 5 6 7 8 9 10
+---------------------
+1 1 1 1 1 1 1 1 1 1
+1 2 2 3 3 4 4 5 5 6
+1 2 2 3 4 5 6 7 8 10
+
+'''
