84fb2b667298d9dc9aa45140445e8136cafc395a
boring-km <kms0644804@naver.com>
2021-02-05 15:39:05 +0900

    [edit] 실전 예제 배낭 알고리즘
    새로운 물건을 추가해도 배낭에 넣을 수 있는 무게라면 일단 넣어보고 비교한다.

--- dp/baekjoon_12865.py
+++ dp/baekjoon_12865.py
@@ -1,25 +1,28 @@
 # https://reakwon.tistory.com/34
 
-# TODO 배낭 알고리즘을 적용해야 한다.
-from itertools import combinations
-
 n, k = map(int, input().split())
-dp = [0] * 100001
-data = [[]]
+data = []
+dp = [[0] * (k+1) for _ in range(n)]
 for i in range(n):
     w, v = map(int, input().split())
     if w <= k:
-        dp[w] = v
         data.append([w, v])
-print(data)
-comb_list = list(combinations([int(_) for _ in range(1, len(data))], 2))
-visit = [0] * len(data)
-for comb in comb_list:
-    l, r = comb[0], comb[1]
-    cur_weight = data[l][0] + data[r][0]
-    cur_value = data[l][1] + data[r][1]
-    if cur_weight <= k:
-        if dp[cur_weight] < dp[data[l][0]] + dp[data[r][0]]:
-            dp[cur_weight] = dp[data[l][0]] + dp[data[r][0]]
+n = len(data)
 
-print(max(dp))
+
+# 배낭 알고리즘
+def knapsack(cur, limit):
+    if cur == n:    # 끝까지 가면
+        return 0
+    weight = dp[cur][limit]
+    if weight != 0:
+        return weight
+    if data[cur][0] <= limit:
+        weight = knapsack(cur + 1, limit - data[cur][0]) + data[cur][1]
+    weight = max(weight, knapsack(cur + 1, limit))
+    dp[cur][limit] = weight
+    return dp[cur][limit]
+
+
+print(knapsack(0, k))
+
