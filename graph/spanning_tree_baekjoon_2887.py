64d6aeb7035cf50e59cbbb38aa997be5517990b0
boring-km <kms0644804@naver.com>
2021-02-12 18:20:51 +0900

    [edit] 그래프 특정 알고리즘 활용 문제 제외

--- graph/spanning_tree_baekjoon_2887.py
+++ graph/spanning_tree_baekjoon_2887.py
@@ -1,46 +1,64 @@
-import sys
-from itertools import permutations
-from collections import defaultdict
-from heapq import *
+# 문제 풀이 실패
 
-input = sys.stdin.readline
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수 입력받기
 n = int(input())
-permu = permutations([int(i) for i in range(n)], 2)
-node_list = []
-for i in range(n):
-    x, y, z = map(int, input().split())
-    node_list.append([x, y, z])
+parent = [0] * (n + 1) # 부모 테이블 초기화
 
+# 모든 간선을 담을 리스트와, 최종 비용을 담을 변수
 edges = []
-for item in permu:
-    left, right = item[0], item[1]
-    l, r = node_list[left], node_list[right]
-    dist = min(abs(l[0]-r[0]), abs(l[1]-r[1]), abs(l[2]-r[2]))
-    edges.append([dist, str(left), str(right)])
+result = 0
 
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, n + 1):
+    parent[i] = i
 
-def prim(first_node):
-    mst = []
-    adj_edges = defaultdict(list)
-    for weight, node1, node2 in edges:
-        adj_edges[node1].append([weight, node1, node2])
-    connected = set(first_node)
-    selected_edge = adj_edges[first_node]
-    heapify(selected_edge)
+x = []
+y = []
+z = []
 
-    while selected_edge:
-        weight, node1, node2 = heappop(selected_edge)
-        if node2 not in connected:
-            connected.add(node2)
-            mst.append([weight, node1, node2])
-            for edge in adj_edges[node2]:
-                if edge[2] not in connected:
-                    heappush(selected_edge, edge)
-    answer = 0
-    for temp_node in mst:
-        answer += temp_node[0]
-    return answer
+# 모든 노드에 대한 좌표 값 입력받기
+for i in range(1, n + 1):
+    data = list(map(int, input().split()))
+    x.append((data[0], i))
+    y.append((data[1], i))
+    z.append((data[2], i))
 
+x.sort()
+y.sort()
+z.sort()
 
-print(prim('0'))
+# 인접한 노드들로부터 간선 정보를 추출하여 처리
+for i in range(n - 1):
+    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+    edges.append((x[i + 1][0] - x[i][0], x[i][1], x[i + 1][1]))
+    edges.append((y[i + 1][0] - y[i][0], y[i][1], y[i + 1][1]))
+    edges.append((z[i + 1][0] - z[i][0], z[i][1], z[i + 1][1]))
 
+# 간선을 비용순으로 정렬
+edges.sort()
+
+# 간선을 하나씩 확인하며
+for edge in edges:
+    cost, a, b = edge
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
