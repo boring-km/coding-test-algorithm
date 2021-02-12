e2e1e1a89e0e09332d606e790421ce6a1cb8dd1a
boring-km <kms0644804@naver.com>
2021-02-12 18:29:17 +0900

    [edit] 아기 상어 문제 풀이 실패 - bfs
    bfs로 최단거리만 탐색한 이후 제일 가까운 위치의 물고기를 x,y 좌표 0,0 부터 찾으면 자연스럽게 왼쪽 위부터 가장 가까운 물고기를 찾을 수 있다.

--- dfs_bfs/bfs_baekjoon_16236.py
+++ dfs_bfs/bfs_baekjoon_16236.py
@@ -1,55 +1,85 @@
+# 문제 풀이 실패 - 풀이 시간 초과
+
 from collections import deque
+INF = 1e9 # 무한을 의미하는 값으로 10억을 설정
+
+# 맵의 크기 N 입력
 n = int(input())
-space = []
-shark_start = []
-move = [[0, -1], [-1, 0], [0, 1], [1, 0]]
-shark_size = 2
-eat_count = 0
-answer = 0
+
+# 전체 모든 칸에 대한 정보 입력
+array = []
 for i in range(n):
-    temp = [int(_) for _ in input().split()]
-    space.append(temp)
-    for j, shark in enumerate(temp):
-        if shark == 9:
-            shark_start = [i, j]
-            space[i][j] = 0
+    array.append(list(map(int, input().split())))
 
+# 아기 상어의 현재 크기 변수와 현재 위치 변수
+now_size = 2
+now_x, now_y = 0, 0
 
-def bfs(start, visit):
-    global eat_count, shark_size, shark_start
-    queue_x = deque([start[1]])
-    queue_y = deque([start[0]])
-    sx, sy = start[1], start[0]
-    visit[sy][sx] = 1
-    while queue_x or queue_y:
-        tx = queue_x.popleft()
-        ty = queue_y.popleft()
-        for g in range(4):
-            x, y = move[g][1] + tx, move[g][0] + ty
-            if 0 <= x < n and 0 <= y < n:   # 공간에서 벗어나지 않으면
-                if shark_size < space[y][x]:    # 더 큰 물고기가 있는 칸은 지나가지 못함
-                    continue
-                if visit[y][x] >= 1:
-                    continue
-                queue_x.append(x)
-                queue_y.append(y)
-                visit[y][x] = visit[ty][tx] + 1  # 방문만 하기
-                if 0 < space[y][x] < shark_size:    # 먹을 수 있는 물고기가 있다면
-                    eat_count += 1
-                    if eat_count == shark_size:
-                        shark_size += 1
-                        eat_count = 0
-                    space[y][x] = 0
-                    shark_start = [y, x]
-                    return visit[y][x] - 1, False
-    return 0, True     # 탐색을 끝까지 했지만 먹을 수 있는 물고기가 없었다면
+# 아기 상어의 시작 위치를 찾은 뒤에 그 위치엔 아무것도 없다고 처리
+for i in range(n):
+    for j in range(n):
+        if array[i][j] == 9:
+            now_x, now_y = i, j
+            array[now_x][now_y] = 0
 
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+# 모든 위치까지의 '최단 거리만' 계산하는 BFS 함수
+def bfs():
+    # 값이 -1이라면 도달할 수 없다는 의미 (초기화)
+    dist = [[-1] * n for _ in range(n)]
+    # 시작 위치는 도달이 가능하다고 보며 거리는 0
+    q = deque([(now_x, now_y)])
+    dist[now_x][now_y] = 0
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                # 자신의 크기보다 작거나 같은 경우에 지나갈 수 있음
+                if dist[nx][ny] == -1 and array[nx][ny] <= now_size:
+                    dist[nx][ny] = dist[x][y] + 1
+                    q.append((nx, ny))
+    # 모든 위치까지의 최단 거리 테이블 반환
+    return dist
+
+# 최단 거리 테이블이 주어졌을 때, 먹을 물고기를 찾는 함수
+def find(dist):
+    x, y = 0, 0
+    min_dist = INF
+    for i in range(n):
+        for j in range(n):
+            # 도달이 가능하면서 먹을 수 있는 물고기일 때
+            if dist[i][j] != -1 and 1 <= array[i][j] < now_size:
+                # 가장 가까운 물고기 한 마리만 선택
+                if dist[i][j] < min_dist:
+                    x, y = i, j
+                    min_dist = dist[i][j]
+    if min_dist == INF: # 먹을 수 있는 물고기가 없는 경우
+        return None
+    else:
+        return x, y, min_dist # 먹을 물고기의 위치와 최단 거리
+
+result = 0 # 최종 답안
+ate = 0 # 현재 크기에서 먹은 양
 
 while True:
-    temp_visit = [[0] * n for _ in range(n)]
-    temp_count, is_end = bfs(shark_start, temp_visit)
-    answer += temp_count
-    if is_end:
+    # 먹을 수 있는 물고기의 위치 찾기
+    value = find(bfs())
+    # 먹을 수 있는 물고기가 없는 경우, 현재까지 움직인 거리 출력
+    if value is None:
+        print(result)
         break
-
-print(answer)
+    else:
+        # 현재 위치 갱신 및 이동 거리 변경
+        now_x, now_y = value[0], value[1]
+        result += value[2]
+        # 먹은 위치에는 이제 아무것도 없도록 처리
+        array[now_x][now_y] = 0
+        ate += 1
+        # 자신의 현재 크기 이상으로 먹은 경우, 크기 증가
+        if ate >= now_size:
+            now_size += 1
+            ate = 0
