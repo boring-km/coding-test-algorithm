9473c10ae7abb2089e7d749dd355b6aafe1253ba
boring-km <kms0644804@naver.com>
2021-03-30 11:40:14 +0900

    [edit] 다른 사람 풀이 확인하기

--- greedy/programmers_safe_boat.py
+++ greedy/programmers_safe_boat.py
@@ -1,47 +1,13 @@
-from collections import deque
-
-
 def solution(people, limit):
-    answer = 0
-    people = deque(sorted(people))
-    cur = 0
+    people.sort()
     count = 0
-    while len(people) > 0:
-        if cur >= limit:
-            answer, count, cur = plus_another_boat(answer)
-            continue
+    i, j = 0, len(people) - 1
+    while i <= j:
         count += 1
-        if cur + people[-1] <= limit:
-            if count == 1:
-                cur += people.pop()
-                if len(people) == 0:
-                    answer += 1
-            else:
-                people.pop()
-                answer, count, cur = plus_another_boat(answer)
-        else:
-            if cur + people[0] <= limit:
-                if count == 1:
-                    cur += people.popleft()
-                    people[0] = 0
-                else:
-                    people.popleft()
-                    answer, count, cur = plus_another_boat(answer)
-            else:
-                answer += 1
-                count = 1
-                cur = people.pop()
-                if len(people) == 0:
-                    answer += 1
-
-    return answer
-
-
-def plus_another_boat(answer):
-    answer += 1
-    count = 0
-    cur = 0
-    return answer, count, cur
+        if people[j] + people[i] <= limit:
+            i += 1
+        j -= 1
+    return count
 
 
 print(solution([10], 100))
