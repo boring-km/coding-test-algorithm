78c1c33bfefa7124911cf2d3a83c0d6956c1446f
boring-km <kms0644804@naver.com>
2021-04-06 13:58:29 +0900

    [edit] 가장 큰 정사각형을 찾는 문제 풀이 통과
    2x2 정사각형 형태로 하나하나 정사각형이 되는지 확인한다.
    탐색한 사각형의 정보를 board에 갱신해가면서 탐색한다. (dp 문제?)
    https://programmers.co.kr/learn/courses/30/lessons/12905?language=python3

--- implement/programmers_find_biggest_rectangle.py
+++ implement/programmers_find_biggest_rectangle.py
@@ -1,26 +1,35 @@
 def solution(board):
-    answer = 0
-    row = len(board)
-    col = len(board[0])
-
-
-    def find(x, y):
-        size = 1
-        while y+size < row and x+size < col:
-            for i in range(y, y+size+1):
-                for j in range(x, x+size+1):
-                    if board[i][j] == 0:
-                        return size
-            size += 1
-        return size
-
-    for y in range(row):
-        for x in range(col):
-            if board[y][x] == 1:
-                result = find(x, y)
-                answer = max(answer, result*result)
-
+    for row in board: # 정답의 최소값이 0인지 1인지 먼저 판별
+        if sum(row):
+            answer = 1
+            break
+    else:
+        return 0
+    # 1행 1열부터 board를 2x2 정사각형으로 탐색하면서 우측 아래 값 최신화
+    # 어차피 값이 1이면 true, 0이면 false
+    row_size = len(board)
+    col_size = len(board[0])
+    for i in range(1, row_size):
+        for j in range(1, col_size):
+            if board[i-1][j-1] and board[i-1][j] and board[i][j-1] and board[i][j]:
+                # 정사각형의 크기를 board에 저장
+                board[i][j] = min(board[i-1][j-1], board[i-1][j], board[i][j-1]) + 1
+                answer = max(answer, board[i][j] ** 2)
     return answer
 
 
-print(solution([[0, 0, 1, 1], [1, 1, 1, 1]]))
+print(solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))
+
+"""
+처음 board
+0 1 1 1
+1 1 1 1
+1 1 1 1
+0 0 1 0
+
+최종 board
+0 1 1 1
+1 1 2 2
+1 2 2 3
+0 0 1 0
+"""
