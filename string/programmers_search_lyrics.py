3b6aa03bf717cad6d3e383568d4cfaeaedc2b55d
boring-km <kms0644804@naver.com>
2021-04-28 11:57:39 +0900

    프로그래머스 - 가사 검색 (효율성 해결)
    ? 문자열을 ? 문자 갯수만큼 a와 z로 채운다음에 사전 순서로 볼 때 그 사이에 들어갈 문자열이 몇개일지 세는 함수로 효율성 해결
    https://programmers.co.kr/learn/courses/30/lessons/60060

--- string/programmers_search_lyrics.py
+++ string/programmers_search_lyrics.py
@@ -1,50 +1,41 @@
+# https://velog.io/@tjdud0123/가사-검색-2020-카카오-공채-python
+from collections import defaultdict
+from bisect import bisect_left, bisect_right
+
+
+def count_by_lange(temp_list, start, end):
+    ## word에서 frozz가 들어갈 인덱스의 우측 인덱스
+    ## word에서 froaa가 들어갈 인덱스의 좌측 인덱스    -> fro??가 있다면 이 두 인덱스 사이에 있는 것이다!!!!
+    left = bisect_right(temp_list, end)
+    right = bisect_left(temp_list, start)
+    return left - right
+
+
 def solution(words, queries):
     answer = []
-    front = dict()
-    back = dict()
-
+    cands = defaultdict(list)
+    reverse_cands = defaultdict(list)
+    # 길이별 저장
     for word in words:
-        f = word[0]
-        b = word[-1]
-
-        if not front.get(f):
-            front[f] = dict()
-            front[f][word] = len(word)
-        else:
-            front[f][word] = len(word)
-        if not back.get(b):
-            back[b] = dict()
-            back[b][word] = len(word)
-        else:
-            back[b][word] = len(word)
-
-    print(front)
-
-    for q in queries:
-        count = 0
-        if q[0] != '?':
-            index = len(q)-1
-            for i in range(len(q)):
-                if q[i] == '?':
-                    index = i-1
-                    break
-            if front.get(q[0]):
-                for key in front[q[0]].keys():
-                    if key[0:index+1] == q[0:index+1] and front[q[0]][key] == len(q):
-                        count += 1
-        else:
-            index = 0
-            for i in range(len(q)):
-                if q[i] != '?':
-                    index = i
-                    break
-            if back.get(q[-1]):
-                for key in back[q[-1]].keys():
-                    if key[index:] == q[index:] and back[q[-1]][key] == len(q):
-                        count += 1
-        answer.append(count)
+        cands[len(word)].append(word)
+        reverse_cands[len(word)].append(word[::-1])
+    # 정렬 O(NlogN)
+    for cand in cands.values():
+        cand.sort()
+    for cand in reverse_cands.values():
+        cand.sort()
+    # 탐색 O(N * logM)
+    for query in queries:
+        if query[0] == '?': # 와일드카드 접두사 일 때
+            temp_list = reverse_cands[len(query)]
+            start, end = query[::-1].replace('?','a'), query[::-1].replace('?','z')
+        else: # 와일드카드 접미사 일 때
+            temp_list = cands[len(query)]
+            start, end = query.replace('?','a'), query.replace('?','z')
+        answer.append(count_by_lange(temp_list, start, end))
 
     return answer
 
 
-print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?", "?????"]))
+
