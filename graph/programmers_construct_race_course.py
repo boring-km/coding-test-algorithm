d5d5d990286f0e47ec976ec60d62810ffd53b96c
boring-km <kms0644804@naver.com>
2021-05-03 21:30:44 +0900

    프로그래머스 - 경주로 건설
    queue를 활용한 BFS 탐색을 사용했다. BFS 탐색을 어디서부터 시작할 지 정해서 비교해야 한다.
    https://programmers.co.kr/learn/courses/30/lessons/67259

--- graph/programmers_construct_race_course.py
+++ graph/programmers_construct_race_course.py
@@ -2,88 +2,28 @@ from collections import deque
 
 
 def solution(board):
-    move = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-
-    start = [0, 0]
-    q = deque([start])
-    record = deque([[]])
     n = len(board)
-    answer = 500 * n * n
 
-    dest = [n - 1, n - 1]
-    visited = [[False for _ in range(n)] for _ in range(n)]
-    visited[0][0] = True
-
-    while q:
-        cur = q.popleft()
-        before = record.popleft()
-
-        if cur[:] == dest[:]:
-            answer = min(answer, board[n-1][n-1])
-        for i in range(4):
-            ty = cur[0] + move[i][0]
-            tx = cur[1] + move[i][1]
-            if cur[0] == 3 and cur[1] == 1:
-                print()
-
-            if 0 <= ty < n and 0 <= tx < n and board[ty][tx] != 1 and (not visited[ty][tx] or board[cur[0]][cur[1]] + 100 <= board[ty][tx]):
-                prev = board[cur[0]][cur[1]]
-                visited[ty][tx] = True
-                # 같은 방향
-                if not before:
-                    board[ty][tx] += 100
-                elif before[0] == before[2]:  # 좌 or 우
-                    if before[3] - before[1] > 0:  # 우측
-                        if i == 0:  # 우측
-                            if board[ty][tx] != 0:
-                                board[ty][tx] = min(prev + 100, board[ty][tx])
-                            else:
-                                board[ty][tx] = prev + 100
-                        else:  # 우측이 아님
-                            if board[ty][tx] == 0:
-                                board[ty][tx] = prev + 600
-                            else:
-                                board[ty][tx] = min(prev + 600, board[ty][tx])
-                    else:
-                        if i == 2:  # 좌측
-                            if board[ty][tx] != 0:
-                                board[ty][tx] = min(prev + 100, board[ty][tx])
-                            else:
-                                board[ty][tx] = prev + 100
-                        else:  # 좌측이 아님
-                            if board[ty][tx] == 0:
-                                board[ty][tx] = prev + 600
-                            else:
-                                board[ty][tx] = min(prev + 600, board[ty][tx])
+    def bfs(start_move):
+        record = [[1e9 for _ in range(n)] for _ in range(n)]
+        move = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        q = deque([0, 0, 0, start_move])
+        record[0][0] = 0
+        while q:
+            y, x, cost, before = q.popleft()
+            for i in range(4):
+                ty = y + move[i][0]
+                tx = x + move[i][1]
+                if i != before:
+                    temp = cost + 600
                 else:
-                    if before[2] - before[0] > 0:  # 하단
-                        if i == 1:
-                            if board[ty][tx] != 0:
-                                board[ty][tx] = min(prev + 100, board[ty][tx])
-                            else:
-                                board[ty][tx] = prev + 100
-                        else:  # 우측이 아님
-                            if board[ty][tx] == 0:
-                                board[ty][tx] = prev + 600
-                            else:
-                                board[ty][tx] = min(prev + 600, board[ty][tx])
-                    else:
-                        if i == 3:
-                            if board[ty][tx] != 0:
-                                board[ty][tx] = min(prev + 100, board[ty][tx])
-                            else:
-                                board[ty][tx] = prev + 100
-                        else:  # 우측이 아님
-                            if board[ty][tx] == 0:
-                                board[ty][tx] = prev + 600
-                            else:
-                                board[ty][tx] = min(prev + 600, board[ty][tx])
-                # 다른 방향
-                q.append([ty, tx])
-                record.append([cur[0], cur[1], ty, tx])
-    for i in range(n):
-        print(board[i])
-    return answer
+                    temp = cost + 100
+                if 0 <= ty < n and 0 <= tx < n and board[ty][tx] == 0 and record[ty][tx] > temp:
+                    record[ty][tx] = temp
+                    q.append([ty, tx, temp, i])
+        return record[n-1][n-1]
+    # 하단으로 먼저 진행, 우측으로 먼저 진행
+    return min(bfs(0), bfs(1))
 
 
 print(solution([[0, 0, 0, 0], [0, 1, 0, 0], [0, 1, 0, 1], [0, 0, 0, 0]]))
